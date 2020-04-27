d995f6ea5a49ce19d2d26c4a73c919b2ccf85575
Akiko Green <akikogreen08@gmail.com>
2020-04-27 02:13:39 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,8 +2,7 @@ def find_min_value(array)
   count = 0 
   min_value = 1  
   while count < array.length do 
-    if min_value == array[count].sort.first
-      min_value = array[count]
+    if 
     end 
   count += 1 
   end
